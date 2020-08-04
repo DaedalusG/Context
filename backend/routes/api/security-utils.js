019da4263dcb8a8201273d374890f528c00353f7
warrgiff <warrenbruceg@gmail.com>
2020-08-04 16:51:50 -0700

    got a server running from backend, only sends errors though

---
+++ backend/routes/api/security-utils.js
@@ -0,0 +1,51 @@
+const bearerToken = require('express-bearer-token');
+const jwt = require('jsonwebtoken');
+const uuid = require('uuid').v4;
+
+const { jwtConfig: { secret, expiresIn } } = require('../../config');
+const UserRepository = require('../../db/user-repository');
+
+function generateToken(user) {
+    const data = {
+        name: user.username,
+    };
+    const jwtid = uuid();
+
+    return {
+        jti: jwtid,
+        token: jwt.sign({ data }, secret, { expiresIn: Number.parseInt(expiresIn), jwtid })
+    };
+}
+
+function restoreUser(req, res, next) {
+    const { token } = req;
+
+    if (!token) {
+        return next({ status: 401, message: 'no token' });
+    }
+
+    return jwt.verify(token, secret, null, async (err, payload) => {
+        if (err) {
+            err.status = 403;
+            return next(err);
+        }
+
+        const tokenId = payload.jti;
+
+        try {
+            req.username = await UserRepository.findByTokenId(tokenId);
+        } catch (e) {
+            return next(e);
+        }
+
+        if (!req.username.isValid()) {
+            return next({ status: 404, message: 'session not found' });
+        }
+
+        next();
+    });
+}
+
+const authenticated = [bearerToken(), restoreUser];
+
+module.exports = { generateToken, authenticated };
--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -2,7 +2,7 @@ const express = require('express');
 const asyncHandler = require('express-async-handler');
 const { check, validationResult } = require('express-validator');
 
-const UserRepository = require('../../db/user-repository')
+const UserRepository = require('../../db/user-repository');
 const { authenticated, generateToken } = require('./security-utils');
 
 const router = express.Router();
@@ -13,35 +13,36 @@ const email =
         .withMessage('Please provide a valid email address')
         .normalizeEmail();
 
-const name =
-    check('name')
-        .not().isEmpty()
-        .withMessage('Please provide a username name');
-
 const password =
     check('password')
         .not().isEmpty()
         .withMessage('Please provide a password');
 
-router.post('/', email, password, name, asyncHandler(async function (req, res, next) {
+router.put('/', [email, password], asyncHandler(async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return next({ status: 422, errors: errors.array() });
     }
 
-    const user = await UserRepository.create(req.body);
-
+    const { email, password } = req.body;
+    const user = await UserRepository.findByEmail(email);
+    if (!user.isValidPassword(password)) {
+        const err = new Error('Login failed');
+        err.status = 401;
+        err.title = 'Login failed';
+        err.errors = ['Invalid credentials'];
+        return next(err);
+    }
     const { jti, token } = generateToken(user);
     user.tokenId = jti;
     await user.save();
     res.json({ token, user: user.toSafeObject() });
 }));
 
-router.get('/me', authenticated, function (req, res) {
-    res.json({
-        email: req.user.email,
-        name: req.user.name,
-    });
-});
+router.delete('/', [authenticated], asyncHandler(async (req, res) => {
+    req.user.tokenId = null;
+    await req.user.save();
+    res.json({ message: 'success' });
+}));
 
 module.exports = router;
